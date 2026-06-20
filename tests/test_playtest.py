55f9b101a41888b4dc74feb327b3da2380d7037b
yanabystrova <byst-yana@mail.ru>
2026-06-20 21:54:00 +0300

    Introduction to Pytest

---
+++ tests/test_playtest.py
@@ -0,0 +1,21 @@
+def test_user_login():
+        print("Hello")
+        
+class TestUserLogin:#
+    def test_1(self):
+       ...
+       
+    def test_2(self):
+        ...
+
+
+def test_assert_positive_case():
+    assert (2 + 2) == 4
+    assert (3 + 3) == 6
+    assert (4 + 4) == 8
+
+
+def test_assert_negative_case():
+    assert (2 + 2) == 5, "(2 + 2) != 5"
+     
+    
