91276e8e4afcc1e02634050d806feced5c9fc04c
yanabystrova <byst-yana@mail.ru>
2026-08-13 09:48:02 +0300

    Adding the test_successful_authorization automated test

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,6 +1,8 @@
 import pytest
 from playwright.sync_api import sync_playwright, Page, Playwright
 
+from pages.authentication.registration_page import RegistrationPage
+
 
 @pytest.fixture
 def chromium_page(playwright: Playwright) -> Page:
@@ -16,32 +18,11 @@ def initialize_browser_state(playwright: Playwright):
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
-
-    page.goto(
-        "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration"
-    )
-
-    email_input = page.get_by_test_id("registration-form-email-input").locator("input")
-    email_input.fill("user.name@gmail.com")
-
-    username_input = page.get_by_test_id("registration-form-username-input").locator(
-        "input"
-    )
-    username_input.fill("username")
-
-    password_input = page.get_by_test_id("registration-form-password-input").locator(
-        "input"
-    )
-    password_input.fill("password")
-
-    registration_button = page.get_by_test_id("registration-page-registration-button")
-    registration_button.click()
-
-    # Дожидаемся завершения регистрации — перехода на страницу Dashboard,
-    # чтобы токен авторизации успел записаться, и только потом сохраняем состояние
-    page.wait_for_url(
-        "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard"
-    )
+    
+    registration_page = RegistrationPage(page=page)
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    registration_page.registration_form.fill(email="user.name@gmail.com", username="username", password="password")
+    registration_page.click_registration_button()
 
     # Сохраняем состояние браузера (storage state) в файл
     context.storage_state(path="browser-state.json")
---
+++ tests/authentication/test_registration.py
@@ -0,0 +1,23 @@
+import pytest
+
+from pages.authentication.login_page import LoginPage
+from pages.authentication.registration_page import RegistrationPage
+from pages.dashboard.dashboard_page import DashboardPage
+
+
+@pytest.mark.regression
+@pytest.mark.authorization
+class TestAuthorization:
+    @pytest.mark.parametrize(
+        "email, password",
+        [
+            ("user.name@gmail.com", "password"),
+            ("user.name@gmail.com", "  "),
+            ("  ", "password")
+        ]
+    )
+    def test_wrong_email_or_password_authorization(self, login_page: LoginPage, email: str, password: str):
+        login_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+        login_page.login_form.fill(email=email, password=password)
+        login_page.click_login_button()
+        login_page.check_visible_wrong_email_or_password_alert()
